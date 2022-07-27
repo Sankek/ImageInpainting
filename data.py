74a0e9d6be4ef1805821d1e686df1b91abddd694
Sankek <iziurov.as@phystech.edu>
2022-07-27 16:31:40 +0300

    Add Dataset class for masks from png files

--- data.py
+++ data.py
@@ -10,20 +10,27 @@ from masks import MaskGenerator
 
 
 class DataFolder(Dataset):
-    def __init__(self, path, dataset_mean=[0.5]*3, dataset_std=[0.5]*3, ext='jpg'):
+    def __init__(self, path, dataset_mean=[0.5]*3, dataset_std=[0.5]*3, ext='.jpg'):
         super().__init__()
         self.path = path
         self.ext = ext
         self.mean = dataset_mean
         self.std = dataset_std
 
-        self.files = list(filter(lambda f: f.endswith(self.ext), os.listdir(self.path)))
+        self.files = list(filter(self.name_filter, os.listdir(self.path)))
         self.len_ = len(self.files)
 
         self.transform = tt.Compose([
             tt.ToTensor(),
             tt.Normalize(self.mean, self.std) 
         ])
+        
+    
+    def name_filter(self, name):
+        if not name.endswith('.' + self.ext):
+            return False
+        
+        return True
 
         
     def __len__(self):
@@ -40,7 +47,7 @@ class DataFolder(Dataset):
     
     
 class InpaintingDataset(DataFolder):
-    def __init__(self, path, image_size, channels=3, dataset_mean=[0.5]*3, dataset_std=[0.5]*3, ext='jpg'):
+    def __init__(self, path, image_size, channels=3, dataset_mean=[0.5]*3, dataset_std=[0.5]*3, ext='.jpg'):
         super().__init__(path, dataset_mean=dataset_mean, dataset_std=dataset_std, ext=ext)
     
         self.image_size = image_size
@@ -56,3 +63,44 @@ class InpaintingDataset(DataFolder):
         mask = torch.FloatTensor(self.mask_generator.generate()).unsqueeze(0).expand(self.channels, -1, -1)
         
         return img*mask, mask, img
+    
+    
+class InpaintingPngMaskDataset(DataFolder):
+    def __init__(self, path, image_size, channels=3, dataset_mean=[0.5]*3, dataset_std=[0.5]*3, ext='jpg', mask_suffix='_mask.png'):
+        super().__init__(path, dataset_mean=dataset_mean, dataset_std=dataset_std, ext=ext)
+        
+        self.mask_suffix = mask_suffix
+        self.image_size = image_size
+        self.channels = channels
+        
+        
+    def name_filter(self, name):
+        if not name.endswith('.' + self.ext):
+            return False
+        if name.endswith(self.mask_suffix):
+            return False
+        
+        return True
+
+        
+    def __getitem__(self, index):
+        name = self.files[index][:-len('.' + self.ext)]
+        mask_name = name + self.mask_suffix
+        
+        file = os.path.join(self.path, self.files[index])
+        mask_file = os.path.join(self.path, mask_name)
+        
+        img = Image.open(file)
+        img = self.transform(img)
+        
+        mask = Image.open(mask_file)
+        mask = np.array(mask)[:, :, 3]
+        valid = mask == 255
+        hole = ~valid
+        mask[valid] = 255
+        mask[hole] = 0
+        mask = torch.FloatTensor(mask/255)
+        mask = mask.unsqueeze(0).expand(self.channels, -1, -1)
+        
+        return img*mask, mask, img
+    
